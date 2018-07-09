aa6d30b84beeecc7f1fcaa8022cbdba9924e7422
Jeremy Parker <jrobinparker@gmail.com>
2018-07-09 14:08:05 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @songs = Artist.songs.find(params[:artist_id]).songs 
     else
+      @songs.all
       flash[:alert] = "Artist not found."
     end 
   end
